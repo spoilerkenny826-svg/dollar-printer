cff840dd2d6a93f6e6a9ca14c9f4e5a6fffa603e
spoilerkenny826-svg <spoilerkenny826@gmail.com>
2026-09-17 23:23:18 +0300

    Add Dollar Printer trading landing page to repository root as a static website.

---
+++ script.js
@@ -0,0 +1,10 @@
+function startTrading() {
+  alert('Trading dashboard coming next.');
+}
+
+function viewMarkets() {
+  document.getElementById('dashboard').scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+}
